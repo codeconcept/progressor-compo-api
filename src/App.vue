9191b4b599279a1b5c2f2b9a84062b1aeb6e56b5
codeconcept <contact@codeconcept.fr>
2021-01-06 17:44:19 +0100

    Cleaning after app creation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Accueil</router-link> |
+    <router-link to="/about">A propos</router-link>
   </div>
   <router-view />
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A propos</h1>
+    <p>Et voilà ;)</p>
   </div>
 </template>
